6cb7fec47ced72cd5ff50ae970def2d3b9634073
torkvenick <oomsnikita@gmail.com>
2020-09-11 06:55:53 +0200

    homework6

--- dz4and5/js/main.js
+++ dz4and5/js/main.js
@@ -2,7 +2,15 @@ $(function(){
 
 
   $('.slider__inner').slick({
-    arrows: true
+    asNavFor: '.thumbs'
+  });
+
+  $('.thumbs').slick({
+    arrows: false,
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    asNavFor: '.slider__inner',
+    focusOnSelect: true
   });
  
 
--- project4/js/main.js
+++ project4/js/main.js
@@ -6,5 +6,12 @@ $(function(){
    dots: true,
  });
 
+ $('.works__inner').slick({
+   slidesToShow: 4,
+   slidesToScroll: 1,
+   prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/slide-left.svg" alt=""></button>',
+   nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/slide-right.svg" alt=""></button>',
+ })
 
-});
+
+}); 
