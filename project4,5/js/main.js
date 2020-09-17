5a5ec90286bc69122a06fe8fde07c6307d304bd5
torkvenick <oomsnikita@gmail.com>
2020-09-17 17:20:08 +0200

    pixel perfect

--- project4,5/js/main.js
+++ project4,5/js/main.js
@@ -13,5 +13,13 @@ $(function(){
    nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/slide-right.svg" alt=""></button>',
  })
 
+ $('.header__menu-btn').on('click', function(){
+  $('.header__menu ul').slideToggle();
+ });
+
+ $('.header__drop-down.drop-down').on('click', function(){
+  console.log('test');
+ });
+
 
 }); 
