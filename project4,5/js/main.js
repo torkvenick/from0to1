a394f1ba3c925eeac11b50d1788c66fc7a9a3a1f
torkvenick <oomsnikita@gmail.com>
2020-09-18 20:56:16 +0200

    adaptiv 1:53:42

--- project4,5/js/main.js
+++ project4,5/js/main.js
@@ -11,6 +11,28 @@ $(function(){
    slidesToScroll: 1,
    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/slide-left.svg" alt=""></button>',
    nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/slide-right.svg" alt=""></button>',
+   responsive: [
+    {
+      breakpoint: 911,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+
+    {
+      breakpoint: 681,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+
+    {
+      breakpoint: 455,
+      settings: {
+        slidesToShow: 1,
+      }
+    },
+  ]
  });
 
  $('.header__menu-btn').on('click', function(){
